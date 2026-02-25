8ea0970d7c71d5e34bbdb0a82d04247a13b26038
khushisingh-22 <ks6637890@gmail.com>
2026-02-26 01:19:47 +0530

    Initial commit:currency convertor

---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+import InputBox from "./INPUTBOX";
+
+export {InputBox};
---
+++ src/hooks/usecurrencyinfo.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+
+// useState → data store karne ke liye
+// useEffect → side effect ke liye (API call, fetch, etc.)
+
+  function useCurrencyinfo(currency){ // custom hook
+    const [data,setData] = useState({})
+  useEffect(() => {
+  fetch(
+      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@latest/currencies/${currency}.json`
+    )
+    .then((res) => res.json()) //usko js object bana deta hai  kyuki server se raw resonse ata h 
+    .then((res) => setData(res[currency]))
+    console.log(data);
+}, [currency])
+   console.log(data);
+   return data;
+  
+}
+export default useCurrencyinfo;
+
+
+// Why console.log shows empty object first?
+// Answer:
+// Because state updates in React are asynchronous, so logging immediately after setState gives old state.
